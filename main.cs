324e647a6654f12bfceec89f1caaa5533e02981d
LanCruz <158656960+LanCruz@users.noreply.github.com>
2024-02-05 17:42:51 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    char money = '0.0';
+
+    switch (money) {
+    case '.01':
+      Console.WriteLine("No Person is found");
+    }
+  }
+}
