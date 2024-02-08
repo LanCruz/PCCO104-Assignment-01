ae543df6acd79d28d61a81892cca7a91aead068b
LanCruz <158656960+LanCruz@users.noreply.github.com>
2024-02-08 13:23:35 +0000

    add money

--- main.cs
+++ main.cs
@@ -3,11 +3,19 @@ using System;
 class Program {
   public static void Main (string[] args) {
 
-    char money = '0.0';
+    decimal money = 0.0m;
 
     switch (money) {
-    case '.01':
+    case '0.0':
       Console.WriteLine("No Person is found");
+      break;
+    case '1':
+      Console.WriteLine("Jose Rizal");
+      break;
+    case '5':
+      Console.WriteLine("Emilio Aguinaldo");
+      break;
+    
     }
   }
 }
