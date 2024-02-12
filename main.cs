7bcfa90f48215d0d5ab39768e32690cde89cc207
LanCruz <158656960+LanCruz@users.noreply.github.com>
2024-02-12 00:10:45 +0000

    added default

--- main.cs
+++ main.cs
@@ -3,44 +3,47 @@ using System;
 class Program {
   public static void Main (string[] args) {
 
-    decimal money = 0.01;
+    decimal money = 500m;
 
     switch (money) {
-    case 0.01:
+    case 0.01m:
       Console.WriteLine("No Person is found");
       break;
-    case 0.05:
+    case 0.05m:
       Console.WriteLine("No Person is found");
       break;
-    case 0.25:
+    case 0.25m:
       Console.WriteLine("No Person is found");
       break;
-    case 1:
+    case 1m:
       Console.WriteLine("Jose Rizal");
       break;
-    case 5:
+    case 5m:
       Console.WriteLine("Emilio Aguinaldo");
       break;
-    case 10:
+    case 10m:
       Console.WriteLine("Andres Bonifacio, Apolinario Mabini");
       break;
-    case 20:
+    case 20m:
       Console.WriteLine("Manuel L. Quezon");
       break;
-    case 50:
+    case 50m:
       Console.WriteLine("Sergio Osmena");
       break;
-    case 100:
+    case 100m:
       Console.WriteLine("Manuel Roxas");
       break;
-    case 200:
+    case 200m:
       Console.WriteLine("Diosdado Macapagal");
       break;
-    case 500:
+    case 500m:
       Console.WriteLine("Benigno Sr. and Corazon Aquino");
       break;
-    case 1000:
+    case 1000m:
       Console.WriteLine("Jose Abad Santos, Vicente Lim, Josefa Llanes Escoda");
+      break;
+    default:
+      Console.WriteLine("Invalid Denomination")
       break;
     }
   }
