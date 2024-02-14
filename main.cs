39d83534899275266829891da9b594bf86f90959
LanCruz <158656960+LanCruz@users.noreply.github.com>
2024-02-14 13:33:01 +0000

    added semi colon

--- main.cs
+++ main.cs
@@ -3,47 +3,47 @@ using System;
 class Program {
   public static void Main (string[] args) {
 
-    decimal money = 500m;
+    decimal money = 150m;
 
     switch (money) {
-    case 0.01m:
-      Console.WriteLine("No Person is found");
+    case .01m:
+      Console.WriteLine("No Person is found in Php. .01");
       break;
-    case 0.05m:
-      Console.WriteLine("No Person is found");
+    case .05m:
+      Console.WriteLine("No Person is found in Php. .05");
       break;
-    case 0.25m:
-      Console.WriteLine("No Person is found");
+    case .25m:
+      Console.WriteLine("No Person is found in Php. .25");
       break;
     case 1m:
-      Console.WriteLine("Jose Rizal");
+      Console.WriteLine("Jose Rizal is found in Php. 1");
       break;
     case 5m:
-      Console.WriteLine("Emilio Aguinaldo");
+      Console.WriteLine("Emilio Aguinaldo is found in Php. 5");
       break;
     case 10m:
-      Console.WriteLine("Andres Bonifacio, Apolinario Mabini");
+      Console.WriteLine("Andres Bonifacio & Apolinario Mabini is found in Php. 10");
       break;
     case 20m:
-      Console.WriteLine("Manuel L. Quezon");
+      Console.WriteLine("Manuel L. Quezon is found in Php. 20");
       break;
     case 50m:
-      Console.WriteLine("Sergio Osmena");
+      Console.WriteLine("Sergio Osmena is found in Php. 50");
       break;
     case 100m:
-      Console.WriteLine("Manuel Roxas");
+      Console.WriteLine("Manuel Roxas is found in Php. 100");
       break;
     case 200m:
-      Console.WriteLine("Diosdado Macapagal");
+      Console.WriteLine("Diosdado Macapagal is found in Php. 200");
       break;
     case 500m:
-      Console.WriteLine("Benigno Sr. and Corazon Aquino");
+      Console.WriteLine("Benigno Sr. and Corazon Aquino is found in Php. 500");
       break;
     case 1000m:
-      Console.WriteLine("Jose Abad Santos, Vicente Lim, Josefa Llanes Escoda");
+      Console.WriteLine("Jose Abad Santos, Vicente Lim, Josefa Llanes Escoda is found in Php. 1000");
       break;
     default:
-      Console.WriteLine("Invalid Denomination")
+      Console.WriteLine("Invalid Denomination");
       break;
     }
   }
